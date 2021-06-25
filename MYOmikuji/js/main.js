a37e40c7687a80c33bc5a7884ea211acd57e4e2d
manamiworld <86469153+manamiworld@users.noreply.github.com>
2021-06-25 18:48:07 +0900

    Add files via upload

---
+++ MYOmikuji/js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+{
+    const btn = document.getElementById('btn');
+    
+    btn.addEventListener('click', () => {
+        const results = ['大吉','中吉','凶','末吉','吉','大凶'];
+        // const n =Math.floor(Math.random() * results.length);
+        // btn.textContent = results[n];
+        // btn.textContent = results[n];
+        const n = Math.random();
+        if (n <0.05) {
+            btn.textContent = '大吉';
+        } else if (n < 0.2) {
+            btn.textContent = '中吉';
+        } else {
+            btn.textContent = '凶';
+        }
+
+    });
+}
+
